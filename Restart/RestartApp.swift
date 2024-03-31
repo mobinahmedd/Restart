4434470d05fb1e53e7742ea1a9bd560201a36af4
Mobin Ahmed <mobinahmed4321@gmail.com>
2024-03-31 02:00:28 -0700

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Apptycoons on 31/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
